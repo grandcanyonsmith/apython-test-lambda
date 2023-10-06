426160ceaf7b4d538b1e0c56107113902910cef6
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 17:43:37 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 def lambda_handler(event, context):
-    url = "https://www.coursecreator360.com/"
+    url = event['body']['website_url']
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     images = soup.find_all('img')
-    image_links = [img['src'] for img in images if 'src' in img.attrs and 'asset' in img['src']]
+    image_links = [img['src'] for img in images if 'src' in img.attrs]
     return {"statusCode": 200, "body": image_links}
