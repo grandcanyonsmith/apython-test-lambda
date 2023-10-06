f4ab63c27c070da21f6806dd0bc0181d9b2e2c77
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 17:50:56 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ def lambda_handler(event, context):
     event_body = json.loads(event['body'])
     url = event_body['website_url']
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser')
     images = soup.find_all('img')
-    image_links = [img['src'] for img in images if 'src' in img.attrs]
-    return {"statusCode": 200, "body": image_links}
+    image_links = [img['src'] for img in images if img.has_attr('src')]
+    return {"statusCode": 200, "body": json.dumps(image_links)}
