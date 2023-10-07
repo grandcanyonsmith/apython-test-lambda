c215c120ac12b066ede28e408d7cc94b1f8a9f0b
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 19:17:42 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,12 +4,12 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3', region_name='us-west-2')
     s3.create_bucket(Bucket='canyon-test-lambda-bucket', CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
     
-    # Create a file and write 'hello world' to it
-    with open('test.txt', 'w') as file:
+    # Create a file in /tmp directory and write 'hello world' to it
+    with open('/tmp/test.txt', 'w') as file:
         file.write('hello world')
     
     # Upload the file to the bucket
-    s3.upload_file('test.txt', 'canyon-test-lambda-bucket', 'test.txt')
+    s3.upload_file('/tmp/test.txt', 'canyon-test-lambda-bucket', 'test.txt')
     
     return {
         'statusCode': 200,
