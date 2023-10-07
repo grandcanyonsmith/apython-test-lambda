b4779740d59581dd8478f091854f3df4da1fc4c1
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 18:28:42 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,4 @@
-import json
-from bs4 import BeautifulSoup
-import requests
+import yfinance as yf
 
 def lambda_handler(event, context):
-    event_body = json.loads(event['body'])
-    url = event_body['website_url']
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    images = soup.find_all('img')
-    image_links = [img['src'] for img in images if img.has_attr('src')]
-    return {"statusCode": 200, "body": json.dumps(image_links)}
+    return {'statusCode': 200, 'body': yf.Ticker("TSLA").info['regularMarketPrice']}
