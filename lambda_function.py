127c50bf97542784c14785e64276828f6cfbe7d3
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 18:40:34 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,16 @@
-import yfinance as yf
+import json
+import requests
 
 def lambda_handler(event, context):
-    return {'statusCode': 200, 'body': yf.Ticker("TSLA").info['regularMarketPrice']}
+    body = json.loads(event['body'])
+    symbol = body['symbol']
+
+    url = "https://alpha-vantage.p.rapidapi.com/query"
+    querystring = {"function":"GLOBAL_QUOTE","symbol":symbol,"datatype":"json"}
+    headers = {
+        "X-RapidAPI-Key": "0519a48ad0msh3652be7be3a1163p1b2223jsn9212f364e710",
+        "X-RapidAPI-Host": "alpha-vantage.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    return {'statusCode': 200, 'body': response.json()}
