25cf2a0f236f8fa044fab0f0f1159ac2aa7ac9f3
Canyon Smith <canyonfsmith@gmail.com>
2023-10-06 19:13:43 -0600

    updated lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.client('s3', region_name='us-west-2')
-    s3.create_bucket(Bucket='canyon-test-lambda-bucket')
+    s3.create_bucket(Bucket='canyon-test-lambda-bucket', CreateBucketConfiguration={'LocationConstraint': 'us-west-2'})
     return {
         'statusCode': 200,
         'body': 'Bucket created successfully'
     }
-#END
